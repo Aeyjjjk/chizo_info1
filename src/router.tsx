2cd576a3e16bd6c4bcd6c166ea04af0f4919cde4
Collins Michael <donotforgetme911@gmail.com>
2026-04-24 11:05:38 +0100

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "./routes/Home";
+import Home from "./routes/home";
 import NotFound from "./routes/NotFound";
 
 export const router = createBrowserRouter([
